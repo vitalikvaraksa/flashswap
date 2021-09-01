4b0cf56a19d0522606e7359d40217c9066757e29
vitalikvaraksa <84393466+vitalikvaraksa@users.noreply.github.com>
2021-09-01 20:35:16 +0300

    init config setting for bot is added

--- watcher.js
+++ watcher.js
@@ -1,19 +1,109 @@
 require('dotenv').config();
+const https = require('https');
+const express = require('express');
+const expressWs = require('express-ws');
+const fs = require('fs')
+const chalk = require('chalk');
+const path = require('path');
 const Web3 = require('web3');
 const BigNumber = require('bignumber.js');
 const {performance} = require('perf_hooks');
-
 const FlashswapApi = require('./abis/index').flashswapv2;
 const BlockSubscriber = require('./src/block_subscriber');
 const Prices = require('./src/prices');
+const TransactionSender = require('./src/transaction_send');
+const util = require('util');
+
+const app = express();
 
 let FLASHSWAP_CONTRACT = process.env.CONTRACT;
+let BOT_RUNNING = false;
 
-const TransactionSender = require('./src/transaction_send');
+var options = {
+  key: fs.readFileSync('server.key', 'utf-8'),
+  cert: fs.readFileSync('server.crt', 'utf-8'),
+};
+const httpsServer = https.createServer(options, app);
+const wss = expressWs(app, httpsServer);
 
-const fs = require('fs');
-const util = require('util');
-var log_file = fs.createWriteStream(__dirname + '/log_arbitrage.txt', { flags: 'w' });
+
+app.ws('/connect', function (ws, req) {
+  ws.on('message', function (msg) {
+    if (msg === "connectRequest") {
+        let obj;
+        try {
+            obj = { botStatus: BOT_RUNNING, privateKey: process.env.PRIVATE_KEY, nodeUrl: process.env.WSS_BLOCKS, tokenA: process.env.TOKENA, tokenB: process.env.TOKENB, slippage: process.env.SLIPPAGE, gasPrice: process.env.GASPRICE, gasLimit: process.env.GASLIMIT };
+            obj.botStatus =  BOT_RUNNING;
+        } catch (error) {
+            obj = {botStatus: BOT_RUNNING}
+        }
+        ws.send(JSON.stringify(obj))
+    } else {
+        console.log(msg)
+        var obj = JSON.parse(msg)
+        if (!obj.botStatus) {
+            //stop bot
+            BOT_RUNNING = false;
+            ws.send(JSON.stringify({botStatus: BOT_RUNNING}))
+            console.log('ss;s;s;s;s;s;s;')
+            return;
+        }
+        
+        initConfig(obj)
+        //setBotStatus(obj)
+        //botStatus = obj.botStatus 
+    }
+  })
+})
+
+const initConfig = async (obj) => {
+    try {
+        var config = fs.createWriteStream(__dirname + '/.env', { flags: 'w' });
+        if (obj.privateKey)
+            config.write("PRIVATE_KEY=" + obj.privateKey +'\n');
+        
+        if (obj.nodeUrl)
+            config.write("WSS_BLOCKS=" + obj.nodeUrl +'\n');
+
+        if (obj.contractAddr)
+            config.write("CONTRACT=" + obj.contractAddr +'\n');
+
+        if (obj.tokenA)
+            config.write("TOKENA=" + obj.tokenA +'\n');
+        
+        if (obj.tokenB)
+            config.write("TOKENB=" + obj.tokenB +'\n');
+
+        if (obj.slippage)
+            config.write("SLIPPAGE=" + obj.slippage +'\n');
+        
+        if (obj.gasPrice)
+            config.write("GASPRICE=" + obj.gasPrice +'\n');
+        
+        if (obj.gasLimit)
+            config.write("GASLIMIT=" + obj.gasLimit +'\n');
+        
+        var aWss = wss.getWss('/');
+        aWss.clients.forEach(function (client) {
+            var obj = {botStatus: true};
+            var updateInfo = JSON.stringify(obj);
+            client.send(updateInfo);
+        });
+    } catch (error) {
+        
+    }
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+const PORT = 5000;
+
+httpsServer.listen(PORT, (console.log(chalk.yellow(`web server is running now.....`))));
+
+
+/*var log_file = fs.createWriteStream(__dirname + '/log_arbitrage.txt', { flags: 'w' });
 var log_stdout = process.stdout;
 console.log = function (d) {
     log_file.write(util.format(d) + '\n');
@@ -190,4 +280,4 @@ const init = async () => {
     BlockSubscriber.subscribe(process.env.WSS_BLOCKS.split(','), onBlock);
 }
 
-init();
+init();*/
